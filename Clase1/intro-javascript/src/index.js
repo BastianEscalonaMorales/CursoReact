36310028ae3b35614439e3343c9b17c4e7bdf10a
Bastián Escalona Morales <bastianescalona@hotmail.com>
2020-12-13 21:12:16 -0300

    domingo 13 dic

--- Clase1/intro-javascript/src/index.js
+++ Clase1/intro-javascript/src/index.js
@@ -1,15 +1,2 @@
 
 
-const activo = true;
-
-/*let mensaje = ''
-
-if (activo){
-    mensaje = 'Activo'
-}else{
-    mensaje= 'Inactivo'
-}
-*/
-
-const mensaje = (activo) ? 'Activo' : 'Inactivo'
-console.log(mensaje)
---
+++ counter-app/src/PrimeraApp.js
@@ -0,0 +1,28 @@
+
+
+const PrimeraApp = ({saludo}) => {
+
+    /*const saludo = {
+        nombre: 'bastian',
+        edad: 20444
+    }
+    */
+
+    return( 
+        <>      
+            <h1> mi wuatona es la mas linda</h1>
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+            <p>{saludo}</p>
+        </>
+    
+
+
+
+     );
+
+
+
+}
+
+export default PrimeraApp;
+

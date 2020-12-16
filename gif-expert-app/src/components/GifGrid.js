8d945b70a10336aab37c5bb2128ad40a020c526d
Bastián Escalona Morales <bastianescalona@hotmail.com>
2020-12-15 21:53:43 -0300

    15 de diciembre

--- gif-expert-app/src/components/GifGrid.js
+++ gif-expert-app/src/components/GifGrid.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { getGifs } from '../helpers/getGif';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({categoria}) => {
 
+    const {data:images,loading} = useFetchGifs(categoria);
 
-    const getGifs = async() =>{
-        const url = `https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=gNHWPEOUUizvbuJD0jqtxDGzKnA6KumO`
-        const resp = await fetch (url);
-        const data = await resp.json();
 
-        console.log(data)
-    
-    
-    }
+
+
+   // getGifs();
     return (
-        <div>
-            <h3>{categoria}</h3>
-        </div>
+        <>
+            <h3 className= "animate__animated animate__fadeIn">{categoria}</h3>
+
+            {loading && <p className="animate__animated animate__flash">Loading</p>}
+            
+        
+                    <div className="card-grid"> 
+                    {
+                        images.map(img =>(
+                            <GifGridItem 
+                                key= {img.id}
+                                {...img}
+                                />
+                        ))
+                    }
+                </div>
+            
+
+         </>
+         
     )
 }
